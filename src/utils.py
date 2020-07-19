0072e9d38afac5c69d7bdb689af06e30d8955aa4
s3nh <steam.panek@gmail.com>
2020-07-19 12:52:32 +0200

    Add utils

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+import Crypto 
+import os 
+import re 
+
+def read_key(path):
+    with open(path) as keyfile:
+        _key = keyfile.open()
+    return _key
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()

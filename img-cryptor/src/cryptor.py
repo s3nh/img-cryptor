6f5f62b73210bef138e48ae11f44e72d6624d7b1
s3nh <panek.damian@icloud.com>
2021-10-17 21:52:05 +0200

    Refactor start

--- img-cryptor/src/cryptor.py
+++ img-cryptor/src/cryptor.py
@@ -1,13 +1,16 @@
+import numpy as np 
 import pathlib
 
 from Crypto.Cipher import AES
 from Crypto import Random 
-from typing import List, Union, Dict, Optional 
+from typing import Union 
 from pathlib import Path
 from PIL import Image
-import numpy as np 
+from typing import TypeVar
 
-class Cryptor():
+Rd = TypeVar('Rd')
+
+class Cryptor(object):
 
     def __init__(self, path : str, outname : str , create: bool = True):
         self.path = path 
@@ -17,10 +20,9 @@ class Cryptor():
         else:
             self._key = kwargs.get('_key')
             self._iv = kwargs.get('_iv')
-       
         self.cipher = self._crt_cipher()
 
-    def _read_image(self):
+    def read_image(self):
         """Read the image based on path argument
 
         Params
@@ -29,16 +31,15 @@ class Cryptor():
 
         Returns
         -----------
+        in_data: np.array, 
 
-        in_data: np.array
-
-
-
+        shape: Tuples
+            Shape of processed image.
         """
         in_data = np.asarray( Image.open(self.path) )
         return in_data
 
-    def _read_data(self):
+    def read_data(self):
         """
         Read data (crypted or encrypted)
         """
@@ -47,15 +48,34 @@ class Cryptor():
         infile.close()
         return data
 
-    def _init_keyiv(self):
+    def write_data(self):
+        encfile = open(self.outname, 'wb')
+        encfile.write(self.enc_data)
+        encfile.close() 
+
+    def initialize_keys(self) -> Union[Rd, Rd]:
         """ Initialize key and initialize vector
         """
         _key = Random.new().read(AES.block_size)
         _iv = Random.new().read(AES.block_size)
         return _key, _iv
 
-    def _crt_cipher(self, algo : str = 'AES'):
-        _cipher = AES.new(self._key, AES.MODE_CFB, self._iv)
+    def create_cipher(self, algo : str = 'AES'):
+        """Create new cipher
+           with predefined algorithm name.
+
+        Params
+        ----------
+
+        algo: str
+            Algorithm name (#TODO list it)
+
+        Returns
+        ----------
+        _cipher: Any
+        """
+        tmp_ciph = getatr(Crypto.Cipher, algo)
+        _cipher = tmp_ciph.new(self._key, AES.MODE_CFB, self._iv)
         return _cipher
 
 class Encryptor(Cryptor):
@@ -67,11 +87,6 @@ class Encryptor(Cryptor):
     def _encrypt(self):
         return self.cipher.encrypt(self.data) 
 
-    def _write_data(self):
-        encfile = open(self.outname, 'wb')
-        encfile.write(self.enc_data)
-        encfile.close() 
-
 class Decryptor(Cryptor):
 
     def __init__(self, path, outname, create, **kwargs):
@@ -81,14 +96,6 @@ class Decryptor(Cryptor):
 
     def _decrypt(self):
         return self.cipher.decrypt(self.data)    
-    
-
-    def _write_data(self):
-        encfile = open(self.outname, 'wb')
-        encfile.write(self.dec_data)
-        encfile.close() 
 
     def _get_numpy(self):
-       return np.frombuffer(self.dec_data, dtype = np.uint8).reshape(kwargs.get('size'))
-    
-    
+       return np.frombuffer(self.dec_data, dtype = np.uint8).reshape()
